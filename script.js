24c17d8aa12637d2cdfd7e09473f856f0af12764
Basudeb Bej <150025362+Basudeb-Bej@users.noreply.github.com>
2024-05-11 19:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-    function clock() {
-      let hour = document.getElementById('hour');
-      let minute = document.getElementById('minute');
-      let seconds = document.getElementById('seconds');
-      let ampm = document.getElementById('ampm');
-
-
-      let h = new Date().getHours();
-      let m = new Date().getMinutes();
-      let s = new Date().getSeconds();
-      var am = 'AM';
-       if (h > 12) {
-          h = h - 12;
-          am = 'PM';
-        }
-
-      h = (h<10) ? '0' + h : h;
-      m = (m<10) ? '0' + m : m;
-      s = (s<10) ? '0' + s : s;
-
-      hour.innerHTML = h;
-      minute.innerHTML = m;
-      seconds.innerHTML = s;
-      ampm.innerHTML = am;
-    };
-   var interval = setInterval(clock, 1000);
+function clock(){
+  const hour = document.getElementById('hour');
+  const minute = document.getElementById('minute');
+  const second = document.getElementById('second');
+  const ampm = document.getElementById('ampm');
+  
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = 'AM'
+     if(h > 12){
+       h = h - 12;
+       am = 'PM';
+     }
+    
+  hour.innerHTML = h < 10 ? '0' + h : h;
+  minute.innerHTML = m < 10 ? '0' + m : m;
+  second.innerHTML = s < 10 ? '0' + s : s;
+    ampm.innerHTML = am;
+}
+clock();
+var interval = setInterval(clock, 1000);
